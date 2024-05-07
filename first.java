2fd21dfddba219ef83c4710a408186efebb2936c
Shivaay <[shivaay251202@gmail.com]>
2024-05-07 14:19:23 +0530

    second

--- first.java
+++ first.java
@@ -2,8 +2,8 @@
 
  class first {
   public static void main(String args[]) {
-    //  int[] numbers = new int[10]; // Array of 10 integers
-      String[] names = {"Alice", "Bob", "Charlie"};
-      System.out.println(names[2]); // Printing the third element of the names array
+     int[] numbers = new int[10]; // Array of 10 integers
+     // String[] names = {"Alice", "Bob", "Charlie"};
+      System.out.println(numbers[2]); // Printing the third element of the names array
   }
 }
